9eecdcb469bb84a2a853118b21618e55d0a5e1d6
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2023-09-27 02:31:18 -0300

    .

--- loans/src/main/java/com/bankApp/loans/LoansApplication.java
+++ loans/src/main/java/com/bankApp/loans/LoansApplication.java
@@ -1,9 +1,32 @@
 package com.bankApp.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Me",
+						email = "myEmail@email.com",
+						url = "someUrl"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Loans microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
